bb8e4b4e90f4072888a4197bdc21c54a95849db3
Apple <apple@Apples-MacBook-Pro.local>
2025-05-29 13:38:43 +0500

    testing github actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Github Actions Running successfully
+            Github Actions testing
           </a>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
